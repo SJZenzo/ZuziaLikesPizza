01a8da8cc9de48375351fd3a2230a6b18a4b1f35
Zuzanna Kowaliszyn <6967368-SuzanPevensive@users.noreply.gitlab.com>
2022-08-02 20:40:53 +0200

    fixes

--- app/src/main/java/kowaliszyn/zuzanna/pizzashare/config/Config.kt
+++ app/src/main/java/kowaliszyn/zuzanna/pizzashare/config/Config.kt
@@ -2,6 +2,5 @@ package kowaliszyn.zuzanna.pizzashare.config
 
 object Config {
     const val NEW_PIZZA_INDEX = -1
-    const val MILLIMETERS_PER_UNIT = 10
     const val DELAY_SPLASH_SCREEN_IN_MILLIS = 2000L
 }
